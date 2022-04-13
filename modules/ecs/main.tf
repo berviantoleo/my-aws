1c1c71207464953a077b0244a25f98c6e3397dc3
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-04-13 08:26:45 +0700

    move ecs resources to specific folder

---
+++ modules/ecs/main.tf
@@ -0,0 +1,134 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.9"
+    }
+  }
+}
+
+resource "aws_default_vpc" "my-personal-web" {
+  tags = {
+    env = "dev"
+  }
+}
+
+resource "aws_default_subnet" "my-personal-web" {
+  availability_zone = "ap-southeast-1a"
+
+  tags = {
+    env = "dev"
+  }
+}
+
+resource "aws_default_subnet" "my-personal-web-1" {
+  availability_zone = "ap-southeast-1b"
+
+
+  tags = {
+    env = "dev"
+  }
+}
+
+resource "aws_security_group" "my-personal-web" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_default_vpc.my-personal-web.id
+
+  ingress {
+    description = "Allow HTTP for all"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "my-personal-web" {
+  name               = "my-personal-web-lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.my-personal-web.id]
+  subnets            = [aws_default_subnet.my-personal-web.id, aws_default_subnet.my-personal-web-1.id]
+  tags = {
+    env = "dev"
+  }
+}
+
+
+resource "aws_lb_target_group" "my-personal-web" {
+  name        = "tf-my-personal-web-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.my-personal-web.id
+}
+
+resource "aws_lb_listener" "my-personal-web" {
+  load_balancer_arn = aws_lb.my-personal-web.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my-personal-web.arn
+  }
+}
+
+
+resource "aws_ecs_cluster" "my-personal-web" {
+  name     = "my-personal-web-api-cluster"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "my-personal-web" {
+  cluster_name = aws_ecs_cluster.my-personal-web.name
+
+  capacity_providers = ["FARGATE"]
+}
+
+resource "aws_ecs_task_definition" "my-personal-web" {
+  family                   = "service"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions = jsonencode([
+    {
+      name      = "my-personal-web-api"
+      image     = "nginx"
+      cpu       = 1024
+      memory    = 2048
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "my-personal-web" {
+  name            = "my-personal-web"
+  cluster         = aws_ecs_cluster.my-personal-web.id
+  task_definition = aws_ecs_task_definition.my-personal-web.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_default_subnet.my-personal-web.id, aws_default_subnet.my-personal-web-1.id]
+    security_groups  = [aws_security_group.my-personal-web.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.my-personal-web.arn
+    container_name   = "my-personal-web-api"
+    container_port   = 80
+  }
+
+  tags = {
+    env = "dev"
+  }
+
+}
