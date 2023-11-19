6d3b4e5b34514b4315844efbc73a728d12267349
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-11-19 19:26:07 +0700

    feat: add deletion policy (#95)
    * feat: add deletion policy
    
    * fix: set default_tags to each provider
    
    * fix: move provider
    
    * fix: add provider

--- main.tf
+++ main.tf
@@ -14,25 +14,6 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  default_tags {
-    tags = {
-      Environment = "Dev"
-    }
-  }
-}
-
-provider "aws" {
-  alias  = "ap-southeast-1"
-  region = "ap-southeast-1"
-}
-
-provider "aws" {
-  alias  = "ap-southeast-3"
-  region = "ap-southeast-3"
-}
-
 # ECR
 resource "aws_ecr_repository" "demo_ecr" {
   provider = aws.ap-southeast-1
@@ -80,6 +61,41 @@ resource "aws_dynamodb_global_table" "note" {
 
   replica {
     region_name = "ap-southeast-1"
+  }
+}
+
+variable "bucket_name" {
+  type     = string
+  nullable = false
+}
+
+
+resource "aws_s3_bucket" "cert_bucket" {
+  provider = aws.ap-southeast-3
+
+  bucket = var.bucket_name
+
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "cert_bucket_lifecycle" {
+  provider = aws.ap-southeast-3
+
+  bucket = aws_s3_bucket.cert_bucket.id
+
+  rule {
+    id = "deletion-rule-1"
+
+    filter {
+      prefix = "merged/"
+    }
+
+    expiration {
+      days = 30
+    }
+
+    # ... other transition/expiration actions ...
+
+    status = "Enabled"
   }
 }
 
