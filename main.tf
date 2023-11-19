7246be5f48857e754871b67e7d4b78aa35464822
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-11-19 17:13:31 +0700

    fix: set default_tags to each provider

--- main.tf
+++ main.tf
@@ -14,8 +14,10 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
+  alias  = "ap-southeast-1"
+  region = "ap-southeast-1"
+
   default_tags {
     tags = {
       Environment = "Dev"
@@ -24,13 +26,14 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "ap-southeast-1"
-  region = "ap-southeast-1"
-}
-
-provider "aws" {
   alias  = "ap-southeast-3"
   region = "ap-southeast-3"
+
+  default_tags {
+    tags = {
+      Environment = "Dev"
+    }
+  }
 }
 
 # ECR
